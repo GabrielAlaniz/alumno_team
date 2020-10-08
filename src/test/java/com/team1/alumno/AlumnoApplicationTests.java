5166c890b188440a1fb755cade3ee0b8b4e45ae9
GabrielAlaniz <gabriel.alaniz89@gmail.com>
2020-10-08 02:55:42 -0300

    initial

---
+++ src/test/java/com/team1/alumno/AlumnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.alumno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
